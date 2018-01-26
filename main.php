d533e858cef7ce4a1b6f9c4f489d940e86e4af81
Spiffyk <oto.stava@gmail.com>
2018-01-26 18:56:54 +0100

    Add classes to main.php

--- main.php
+++ main.php
@@ -9,3 +9,5 @@ require_once __DIR__ . "/core/mem.class.php";
 // Core data classes
 require_once __DIR__ . "/core/data/usergroup.class.php";
 require_once __DIR__ . "/core/data/user.class.php";
+require_once __DIR__ . "/core/data/article.class.php";
+require_once __DIR__ . "/core/data/user_approval.class.php";
