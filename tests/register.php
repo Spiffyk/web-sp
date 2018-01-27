3c6958e3ab6cfa8ba139df75d5241a754d843ac1
Spiffyk <oto.stava@gmail.com>
2018-01-27 12:48:05 +0100

    Remove tests (not needed as we have a frontend now)

--- tests/register.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if (!empty($_POST["action"]) && $_POST["action"] == "register") {
-        require_once __DIR__."/register/register.php";
-    } else {
-        require_once __DIR__."/register/form.php";
-    }
-?>
