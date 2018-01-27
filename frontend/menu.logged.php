7ce48e4e2a68f9fa471b6e3957bba469781c521a
Spiffyk <oto.stava@gmail.com>
2018-01-27 13:24:30 +0100

    Add system config menu item

--- frontend/menu.logged.php
+++ frontend/menu.logged.php
@@ -1,5 +1,14 @@
+<?php
+$session = Session::getInstance();
+?>
+
 <ul>
     <li class="username-display"><strong><?php echo Session::getInstance()->getUser()->getName(); ?></strong></li>
+    <?php
+    if ($session->getGroup()->hasPermission("config")) {
+        ?> <li class="configuration-link"><a href="?action=sysconf">Systémová nastavení</a></li> <?php
+    }
+    ?>
     <li class="userarticles-link"><a href="?action=userarticles">Moje články</a></li>
     <li class="logout-link"><a href="?cmd=logout">Odhlásit se</a></li>
 </ul>
