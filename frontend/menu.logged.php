74316a9821aff16857e9af4dba2e4f384b18296c
Spiffyk <oto.stava@gmail.com>
2018-01-27 14:19:40 +0100

    Menu adjustments

--- frontend/menu.logged.php
+++ frontend/menu.logged.php
@@ -1,14 +1,23 @@
 <?php
 $session = Session::getInstance();
+$group = $session->getGroup();
 ?>
 
 <ul>
-    <li class="username-display"><strong><?php echo Session::getInstance()->getUser()->getName(); ?></strong></li>
+    <li><strong><?php echo Session::getInstance()->getUser()->getName(); ?></strong></li>
     <?php
-    if ($session->getGroup()->hasPermission("config")) {
-        ?> <li class="configuration-link"><a href="?action=sysconf">Systémová nastavení</a></li> <?php
+    if ($group->hasPermission("control_panel")) {
+        ?> <li><a href="?action=cpanel">Ovládací panel</a></li> <?php
+    }
+
+    if ($group->hasPermission("user_approval")) {
+        ?> <li><a href="?action=user-approval">Uživatelé ke schválení</a></li> <?php
+    }
+
+    if ($group->hasPermission("article_approval")) {
+        ?> <li><a href="?action=article-approval">Články ke schválení</a></li> <?php
     }
     ?>
-    <li class="userarticles-link"><a href="?action=userarticles">Moje články</a></li>
-    <li class="logout-link"><a href="?cmd=logout">Odhlásit se</a></li>
+    <li><a href="?action=userarticles">Moje články</a></li>
+    <li><a href="?cmd=logout">Odhlásit se</a></li>
 </ul>
