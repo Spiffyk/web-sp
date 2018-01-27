35ee3941bf670b2fb8027ead39ad90fe8bb8adb2
Spiffyk <oto.stava@gmail.com>
2018-01-27 13:58:37 +0100

    Add registration form

--- frontend/action/register.php
+++ frontend/action/register.php
@@ -1,5 +1,32 @@
 <div class="userform">
     <?php
+    $session = Session::getInstance();
 
+    if ($session->isActive()) {
+        ?>
+
+        Přihlášen jako <em><?php echo $session->getUser()->getName() ?></em>.<br />
+        Registrace je povolena jen nepřihlášeným uživatelům.
+
+        <?php
+    } else {
+        ?>
+
+        <h2>Registrace</h2>
+
+        <form method="post">
+            <input type="hidden" name="cmd" value="register">
+
+            <label>Uživatelské jméno<br /><input type="text" name="username"></label><br /><br />
+            <label>E-mail<br /><input type="email" name="email"></label><br /><br />
+            <label>Heslo<br /><input type="password" name="password"></label><br /><br />
+            <label>Heslo (potvrzení)<br /><input type="password" name="password-confirm"></label><br /><br />
+
+            <input type="submit" value="Odeslat registraci ke schválení">
+
+        </form>
+
+        <?php
+    }
     ?>
 </div>
