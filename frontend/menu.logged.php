283e287d4be327ba323d3d770e7a00990f2c6dff
Spiffyk <oto.stava@gmail.com>
2018-01-29 18:11:14 +0100

    Add approval count in the menu

--- frontend/menu.logged.php
+++ frontend/menu.logged.php
@@ -7,7 +7,19 @@ $group = $session->getGroup();
     <li><strong><?php echo Session::getInstance()->getUser()->getName(); ?></strong></li>
     <?php
     if ($group->hasPermission(Permissions::USER_APPROVAL)) {
-        ?> <li><a href="?action=user-approval">Uživatelé ke schválení</a></li> <?php
+        ?>
+
+        <li>
+            <a href="?action=user-approval">Uživatelé ke schválení</a>
+            <?php
+                $count = UserApproval::dao_countOpen();
+                if ($count > 0) {
+                    echo "(" . $count . ")";
+                }
+            ?>
+        </li>
+
+        <?php
     }
 
     if ($group->hasPermission(Permissions::ARTICLE_APPROVAL)) {
