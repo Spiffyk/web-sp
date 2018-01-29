33577f1279081128543fe417c1373d94bbb8f635
Spiffyk <oto.stava@gmail.com>
2018-01-29 20:41:54 +0100

    Implement homepage

---
+++ frontend/action/home/articles.php
@@ -0,0 +1,86 @@
+<?php
+const ARTICLES_PER_PAGE = 20;
+
+if (empty($_GET["page"])) {
+    $current_page = 0;
+} else {
+    $current_page = $_GET["page"];
+}
+
+$no_of_articles = Article::dao_countPublic();
+
+if ($no_of_articles == 0) {
+    ?> Zatím zde není žádný příspěvek. <?php
+} else {
+    $no_of_pages = ceil($no_of_articles / ARTICLES_PER_PAGE);
+
+    $articles = Article::dao_getNewestPublic(ARTICLES_PER_PAGE, $current_page * ARTICLES_PER_PAGE);
+
+    ?>
+
+    <table>
+        <colgroup>
+            <col style="width: 1%">
+            <col>
+            <col style="width: 1%">
+            <col style="width: 1%">
+            <col style="width: 1%">
+        </colgroup>
+        <thead>
+        <tr>
+            <td>Titulek</td>
+            <td>Abstrakt</td>
+            <td>Datum</td>
+            <td>Recenze</td>
+            <td>Stáhnout PDF</td>
+        </tr>
+        </thead>
+        <tbody>
+
+        <?php
+        foreach ($articles as $article) {
+            ?>
+
+            <tr>
+                <td><?php echo $article->getTitle(); ?></td>
+                <td class="wrap"><?php echo $article->getAbstract(); ?></td>
+                <td>
+
+                    <?php
+                    if (empty($article->getModified())) {
+                        $date = $article->getCreated();
+                    } else {
+                        $date = $article->getModified();
+                    }
+
+                    echo $date->format("Y-m-d");
+                    ?>
+
+                </td>
+                <td><a href="?action=article-reviews&article=<?php echo $article->getId(); ?>">Recenze</a></td>
+                <td><a href="/<?php echo $article->getFile(); ?>">Stáhnout PDF</a></td>
+            </tr>
+
+            <?php
+        }
+        ?>
+
+        </tbody>
+    </table>
+
+    <div class="pager">
+
+        <?php
+        for ($i = 0; $i < $no_of_pages; $i++) {
+            if ($i == $current_page) {
+                echo "<strong>" . ($i + 1) . "</strong> ";
+            } else {
+                echo "<a href=\"?action=user-articles&page=" . $i . "\">" . ($i + 1) . "</a> ";
+            }
+        }
+        ?>
+
+    </div>
+
+    <?php
+}
---
+++ frontend/action/home/welcome.php
@@ -0,0 +1,1 @@
+Vítejte v konferenčním systému. Vyberte prosím jednu z položek v nabídce výše.
